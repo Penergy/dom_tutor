0ba18e132d8d1f747c1f4da39fd629df0e9e86c8
Eason Peng <pengyc@rc.inesa.com>
2017-11-29 18:12:36 +0800

    message info

--- js/displayCitations.js
+++ js/displayCitations.js
@@ -1,11 +1,31 @@
-function displayCitations() {
-    var quotes = document.getElementsByTagName("blockquote");
-    var cites = new Array();
-    if (quotes.length < 1) return false;
-    // 获取引用
-    for (var i = 0; i < quotes.length; i++) {
-        cites[i] = quotes[i].getAttribute("cite");
-    }
-    // 
+addLoadEvent(displayCitations);
 
+function displayCitations() {
+    if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) {
+        return false;
+    }
+    // get all citations
+    var quotes = document.getElementsByTagName("blockquote");
+    // iterate all citations
+    for (var i = 0; i < quotes.length; i++) {
+        // if no cite than continue
+        if (!quotes[i].getAttribute("cite")) continue;
+        // save citation
+        var url = quotes[i].getAttribute("cite");
+        //
+        var quoteChildren = quotes[i].getElementsByTagName("*");
+        //
+        if (quoteChildren.length < 1) continue;
+        //
+        var elem = quoteChildren[quoteChildren.length -1];
+        // 
+        var link = document.createElement("a");
+        var link_text = document.createTextNode("source");
+        link.appendChild(link_text);
+        link.setAttribute("href",url);
+        var superscript = document.createElement("sup");
+        superscript.appendChild(link);
+        //
+        elem.appendChild(superscript);
+    }
 }
